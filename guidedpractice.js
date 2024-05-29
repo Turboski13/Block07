b1df6fa336412c20dd6fd1c7049e2e92e21d746d
Jessica Lafferty <jessica.a.lafferty@gmail.com>
2024-05-28 20:55:45 -0400

    workshop for block 07

---
+++ guidedpractice.js
@@ -0,0 +1,27 @@
+
+/* 1. Grab a toothbrush with your hand.
+2. Turn on the water.
+3. Rinse the brush part of the toothbrush with water.
+4. Turn off the water.
+5. Open the tube of toothpaste.
+6. Squeeze toothpaste on the brush part of the toothbrush.
+7. Start with the front teeth.
+8. Move in circular motions with the brush. */
+
+let numCows = 50;
+
+const greeting = "Hello World";
+
+console.log("Hello, " + "Disney Land!");
+
+const numUmbrellas = 10 * 3;
+
+var num = 5 + 1;
+console.log(num);
+
+const str = 'Hello' + ' ' + 'World';
+
+let str1 = "Hello,";
+let str2 = "I am";
+let name = "Josh";
+alert(str1 + " " + str2 + " " + name);
---
+++ workshop.js
@@ -0,0 +1,13 @@
+/* random number generator here */
+let number = 6; 
+
+/* Define variables here */
+let var1 = number + 5
+let var2 = number / 2
+let var3 = number - 3
+
+/* and now to get the printout */
+const runThatNoise = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${var1} ${var2} ${var3}`
+console.log(runThatNoise);
+/* and now script to get a dialog box */
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${var1} ${var2} ${var3}");
